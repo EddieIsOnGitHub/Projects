d6e2078268add377bb41b4704f211d26be58ce92
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-03-23 19:47:52 +0200

    new files

--- Project_2/examples/examples.js
+++ Project_2/examples/examples.js
@@ -89,12 +89,52 @@
 
 
 
-fetch('https://example.com/data')
-    .then(response => response.json())
-    .then(data => processData(data))
-    .then(processedData => {
-        // do something with the processed data
-    })
-    .catch(error => console.log(error))
+// fetch('https://example.com/data')
+//     .then(response => response.json())
+//     .then(data => processData(data))
+//     .then(processedData => {
+//         // do something with the processed data
+//     })
+//     .catch(error => console.log(error))
 
 
+// fetch("https://api.github.com/users/octocat")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       try {
+//         //processing received data
+//         console.log(data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     })
+//     .catch((error) => console.log(error));
+
+
+// let promise1 = fetch('https://jsonplaceholder.typicode.com/posts/1');
+// let promise2 = fetch('https://jsonplaceholder.typicode.com/posts/2');
+// let promise3 = fetch('https://jsonplaceholder.typicode.com/posts/3');
+
+// Promise.all([promise1, promise2, promise3])
+// .then((values) => {
+//   console.log(values);
+// })
+
+// fetch('https://some-api.com/data')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+
+// async function getData() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//     const data = await response.json();
+//     console.log(data);
+//   }
+  
+//   getData();
+  
